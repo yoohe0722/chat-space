3f71239aba20452a5c8cd4ec8e67c9bba3d1cc73
Yohei Higashi <yoohe0722@gmail.com>
2019-08-23 21:52:31 +0900

    add_message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,27 @@
 $(function(){
+  function buildHTML(message){
+    var html =`<div class="message">
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="lower-message">
+                  <p class="lower-message__content">
+                    ${message.content}
+                  </p>
+
+                </div>
+              </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -11,4 +30,17 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.messages').append(html)
+      $('#message_text').val('')
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('エラー')
+    })
+    .always(() => {
+      $(".form__submit").removeAttr("disabled");
+      });
   })
+});
