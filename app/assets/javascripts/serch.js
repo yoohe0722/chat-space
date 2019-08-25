98ec387900a2978d5e39818def7c6381cde18669
Yohei Higashi <yoohe0722@gmail.com>
2019-08-25 21:50:18 +0900

    create_functtion_addMember

--- app/assets/javascripts/serch.js
+++ app/assets/javascripts/serch.js
@@ -1,11 +1,12 @@
 $(function() {
 
   var search_result = $("#user-search-result");
+  var add_member = $("#user-added-member");
 
   function appendUser(user) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
                 search_result.append(html);
   }
@@ -13,6 +14,16 @@ $(function() {
     var html = `<div class='chat-group-user__name'>${ msg }</div>`
     search_result.append(html);
   }
+
+  function addMember(user_id,user_name) {
+    var html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+                add_member.append(html);
+  }
+
 
   $(".username__input").on("keyup", function() {
     var input = $(".username__input").val();
@@ -37,7 +48,16 @@ $(function() {
       alert('ユーザー検索に失敗しました');
     })
   });
+
   $(document).on("click", ".chat-group-user__btn--add", function(){
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+    $(this).parent().remove();
+    addMember(user_id,user_name);
+  })
+
+  $(document).on("click", ".chat-group-user__btn--remove", function(){
     $(this).parent().remove();
   })
+
 });
