27a6cead8c56fb1c6871f6e8f39d0d23661e9b8b
Yohei Higashi <yoohe0722@gmail.com>
2019-08-25 21:48:18 +0900

    edit_UsersControler

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where('name LIKE(?)', "#{params[:keyword]}%").limit(20)
+    @users = User.where('name LIKE(?)', "#{params[:keyword]}%")
+    
     respond_to do |format|
       format.json
     end
