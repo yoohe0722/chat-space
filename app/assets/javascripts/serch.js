3b240ee53fe2567a29fefc417c3056804051c94f
Yohei Higashi <yoohe0722@gmail.com>
2019-08-25 15:39:57 +0900

    edit_js

---
+++ app/assets/javascripts/serch.js
@@ -0,0 +1,44 @@
+$(function() {
+
+  // ↓自信ないやつ
+  var search_result = $("#user-search-result");
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                </div>`
+                search_result.append(html);
+  }
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class='chat-group-user__name'>${ msg }</div>`
+    search_result.append(html);
+  }
+// ↑ここまで
+
+  $(".username__input").on("keyup", function() {
+    var input = $(".username__input").val();
+    console.log(input);
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users) {
+      console.log(users);
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        })
+      } else {
+        console.log("testtest");
+        appendErrMsgToHTML("一致するユーザーはいません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
+  });
+});
